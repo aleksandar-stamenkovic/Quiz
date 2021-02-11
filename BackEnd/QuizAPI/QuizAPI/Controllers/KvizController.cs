9adfaa58fc332558f9f2f1302a7e501cbaadceb9
aleksandar <acaacaas@gmail.com>
2021-02-11 19:21:56 +0100

    backend objectId introduced, proveri, ...

--- BackEnd/QuizAPI/QuizAPI/Controllers/KvizController.cs
+++ BackEnd/QuizAPI/QuizAPI/Controllers/KvizController.cs
@@ -26,8 +26,8 @@ namespace QuizAPI.Controllers
         {
             var db = client.GetDatabase("quiz");
             var collection = db.GetCollection<Korisnik>("korisnici");
-            kviz.Id = DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString();
-
+            // kviz.Id = DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString();
+            kviz.Id = ObjectId.GenerateNewId();
             var filter = Builders<Korisnik>.Filter.Eq(x => x.Email, email);
             var update = Builders<Korisnik>.Update.Push(x => x.Kvizovi, kviz);
             await collection.FindOneAndUpdateAsync(filter, update);
@@ -42,11 +42,11 @@ namespace QuizAPI.Controllers
             var db = client.GetDatabase("quiz");
             var collection = db.GetCollection<Korisnik>("korisnici");
 
-
-            var filter = Builders<Korisnik>.Filter.ElemMatch(x => x.Kvizovi, x => x.Id == id);
+            var objId = ObjectId.Parse(id);
+            var filter = Builders<Korisnik>.Filter.ElemMatch(x => x.Kvizovi, x => x.Id == objId);
             var korisnik = await collection.Find(filter).FirstOrDefaultAsync();
 
-            return korisnik.Kvizovi.FirstOrDefault(x => x.Id == id);
+            return korisnik.Kvizovi.FirstOrDefault(x => x.Id == objId);
         }
 
         [HttpGet("email/{email}")]
@@ -66,14 +66,53 @@ namespace QuizAPI.Controllers
 
         [HttpPost("ucesnik/{id}")]
         // Dodaje ucesnika na osnovu id-a kviza
-        public async Task DodajUcesnika(Ucesnik ucesnik, string id)
+        public async Task DodajUcesnika([FromBody]Ucesnik ucesnik, [FromRoute]string id)
         {
             var db = client.GetDatabase("quiz");
             var collection = db.GetCollection<Korisnik>("korisnici");
 
-            var filter = Builders<Korisnik>.Filter.ElemMatch(x => x.Kvizovi, x => x.Id == id);
+            var objId = ObjectId.Parse(id);
+            var filter = Builders<Korisnik>.Filter.ElemMatch(x => x.Kvizovi, x => x.Id == objId);
             var update = Builders<Korisnik>.Update.Push("Kvizovi.$.Ucesnici", ucesnik);
             await collection.FindOneAndUpdateAsync(filter, update);
+
+        }
+
+        [HttpDelete("{id}")]
+        // Brisanje kviza na osnovu id-a
+        public async Task ObrisiKviz(string id)
+        {
+            var db = client.GetDatabase("quiz");
+            var collection = db.GetCollection<Korisnik>("korisnici");
+
+            var objId = ObjectId.Parse(id);
+            var filter = Builders<Korisnik>.Filter.ElemMatch(x => x.Kvizovi, x => x.Id == objId);
+            var update = Builders<Korisnik>.Update.PullFilter(x => x.Kvizovi, x => x.Id == objId);
+            await collection.FindOneAndUpdateAsync(filter, update);
+        }
+
+        [HttpGet("proveri/{id}")]
+        // Proverava da li postoji kviz sa zadatim id-jem
+        public async Task<bool> Proveri(string id)
+        {
+            var db = client.GetDatabase("quiz");
+            var collection = db.GetCollection<Korisnik>("korisnici");
+
+            bool ok = ObjectId.TryParse(id, out var objId);
+            if (!ok)
+                return false;
+
+            var filter = Builders<Korisnik>.Filter.ElemMatch(x => x.Kvizovi, x => x.Id == objId);
+            var korisnik = await collection.Find(filter).FirstOrDefaultAsync();
+            if (korisnik == null)
+                return false;
+
+            var kviz = korisnik.Kvizovi.FirstOrDefault(x => x.Id == objId);
+
+            if (kviz != null)
+                return true;
+            else
+                return false;
 
         }
 
